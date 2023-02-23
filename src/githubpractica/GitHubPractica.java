31d3ff4e0ce21fd8a4c0a7847f8dbf8e88ccde70
minari05 <trujilloximena11@gmail.com>
2023-02-23 08:22:29 -0600

    Estas es la primer version

---
+++ src/githubpractica/GitHubPractica.java
@@ -0,0 +1,18 @@
+
+package githubpractica;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ximer
+ */
+public class GitHubPractica {
+
+    
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null, "Hola Mundo:)");
+    }
+    
+}
